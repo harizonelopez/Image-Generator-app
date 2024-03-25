4ea8e48d29e916faf9fff640c19b0d7dd96977b0
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-03-25 15:43:11 +0300

    created the main app and the templates

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-prin
+
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+app.secret_key = 'aladinh00-01montext'
+
+def get_random_image():
+    url = "https://api.unsplash.com/photos/random?client_id=-IFMpEhqG-4N4zuvG4MGoxgsk727kFry9zDIjGqHm0I"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        data = response.json()
+        image_url = data.get("urls", {}).get("regular")
+        return image_url
+    
+    except Exception as e:
+        print(f"Error fetching meme: {e}")
+        
+        return None
+
+@app.route('/')
+def home():
+    image_url =  get_random_image()
+    return render_template("home.html", image_url=image_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
